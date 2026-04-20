10d972bcace32ee8f777a75ab1408b9205c94b40
devdilshan2025 <dsbandara4@gmail.com>
2026-04-20 08:28:50 +0530

    update place holder

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,14 +11,28 @@ import { CommonModule } from '@angular/common';
   styleUrl: './app.css'
 })
 export class AppComponent {
-  user = { name: '', email: '', password: '', weight: 0, height: 0 };
+  // මෙතන weight සහ height වලට 0 වෙනුවට null දැම්මා 
+  // එතකොට තමයි HTML එකේ placeholder එක ලස්සනට පේන්නේ
+  user = {
+    name: '',
+    email: '',
+    password: '',
+    weight: null, 
+    height: null
+  };
+
   constructor(private userService: UserService) {}
 
   onRegister() {
     this.userService.registerUser(this.user).subscribe({
-      next: (res) => alert("User Registered Successfully!"),
+      next: (res) => {
+        alert("User Registered Successfully!");
+        console.log(res);
+        // Register වුණාට පස්සේ form එක clear කරන්න ඕනේ නම් මේක පාවිච්චි කරන්න පුළුවන්:
+        // this.user = { name: '', email: '', password: '', weight: null, height: null };
+      },
       error: (err) => {
-        alert("Registration Failed!");
+        alert("Registration Failed! Check Console.");
         console.error(err);
       }
     });
