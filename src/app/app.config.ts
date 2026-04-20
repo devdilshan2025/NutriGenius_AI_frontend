41d7cfd952fe0ed125b6c79998c54bacd0ef32e2
devdilshan2025 <dsbandara4@gmail.com>
2026-04-20 08:07:40 +0530

    Final Fix: All configurations and Registration form completed

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,6 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http'; // මේක අලුතින් එකතු කළා
+import { provideHttpClient } from '@angular/common/http'; 
 
 import { routes } from './app.routes';
 
@@ -8,6 +8,6 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }), 
     provideRouter(routes),
-    provideHttpClient() // HttpClient එක මෙතනට දාන්න
+    provideHttpClient() 
   ]
 };
