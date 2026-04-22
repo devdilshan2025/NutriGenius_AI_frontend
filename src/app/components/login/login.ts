b602736f837ba056724ed26bb0989f8fdf4e972f
devdilshan2025 <dsbandara4@gmail.com>
2026-04-22 10:18:36 +0530

    Logints updated

--- src/app/components/login/login.ts
+++ src/app/components/login/login.ts
@@ -2,25 +2,31 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { UserService } from '../../service/user.service';
+import { Router, RouterModule } from '@angular/router'; // RouterModule මෙතන තියෙන්න ඕනේ
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, RouterModule], // මෙතනට RouterModule අනිවාර්යයි
   templateUrl: './login.html',
   styleUrl: './login.css'
 })
 export class LoginComponent {
-  loginData = {
-    email: '',
-    password: ''
-  };
+  loginData = { email: '', password: '' };
 
-  constructor(private userService: UserService) {}
+  constructor(private userService: UserService, private router: Router) {}
 
   onLogin() {
-    console.log("Logging in with:", this.loginData);
-    // දැනට ඇලර්ට් එකක් විතරක් දාමු වැඩද බලන්න
-    alert("Login function coming soon! Email: " + this.loginData.email);
+    this.userService.loginUser(this.loginData).subscribe({
+      next: (res: string) => {
+        if (res === "Login Successful!") {
+          // Dashboard එකට Navigate කරනවා
+          this.router.navigate(['/dashboard']); 
+        } else {
+          alert(res);
+        }
+      },
+      error: (err) => alert("Backend error!")
+    });
   }
 }
