d59c38a516e3650b7a4649df32a4c055d2261ab3
devdilshan2025 <dsbandara4@gmail.com>
2026-04-29 18:45:11 +0530

    diat plan ts updated

--- src/app/components/diet-plan/diet-plan.ts
+++ src/app/components/diet-plan/diet-plan.ts
@@ -1,9 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { UserService } from '../../service/user.service';
 
 @Component({
   selector: 'app-diet-plan',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './diet-plan.html',
-  styleUrl: './diet-plan.css',
+  styleUrl: './diet-plan.css'
 })
-export class DietPlan {}
+export class DietPlanComponent implements OnInit { // නම 'DietPlanComponent' ලෙස වෙනස් කළා
+  dietPlan: string = "";
+  isLoading: boolean = true;
+  userBmiStatus: string = "";
+
+  constructor(private userService: UserService) {}
+
+  ngOnInit() {
+    const email = localStorage.getItem('userEmail');
+    if (email) {
+      this.userService.getDietPlan(email).subscribe({
+        next: (res: any) => {
+          this.dietPlan = res.plan;
+          this.userBmiStatus = res.status;
+          this.isLoading = false;
+        },
+        error: (err: any) => {
+          console.error("Diet Plan Error:", err);
+          this.dietPlan = "සමාවෙන්න, Diet Plan එක ලබා ගැනීමට නොහැකි විය.";
+          this.isLoading = false;
+        }
+      });
+    }
+  }
+}
